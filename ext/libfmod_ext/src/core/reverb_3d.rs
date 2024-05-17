2c8c004a84ae8b4fc5c8406e40fa62ab24b02a74
Speak2Erase <lily@nowaffles.com>
2024-05-17 07:43:23 -0700

    Reverb3D binding

--- ext/libfmod_ext/src/core/reverb_3d.rs
+++ ext/libfmod_ext/src/core/reverb_3d.rs
@@ -8,7 +8,7 @@ use crate::{Bindable, Result};
 
 use crate::{extern_struct, extern_struct_bind, extern_struct_fns};
 
-use super::structs::Vector;
+use super::structs::{ReverbProperties, Vector};
 
 // TODO subclass channelcontrol
 extern_struct! {
@@ -19,12 +19,24 @@ extern_struct_fns! {
   impl Reverb3D {
     fn set_3d_attributes(position: Option<Vector>, min_distance: f32, max_distance: f32) -> ();
     fn get_3d_attributes() -> (Vector, f32, f32);
+    fn set_properties(properties: ReverbProperties) -> ();
+    fn get_properties() -> ReverbProperties;
+    fn set_active(active: bool) -> ();
+    fn get_active() -> bool;
+    // TODO userdata
+    fn release() -> ();
   }
 }
 
 extern_struct_bind! {
   impl Bindable for Reverb3D: fmod::Reverb3D {
-
+    fn set_3d_attributes -> 3;
+    fn get_3d_attributes -> 0;
+    fn set_properties -> 1;
+    fn get_properties -> 0;
+    fn set_active -> 1;
+    fn get_active -> 0;
+    fn release -> 0;
   }
 }
 
